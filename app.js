de33b5502cafc017c722995d62dbf1271fd854f3
lutvianes <lutvianes@gmail.com>
2016-09-05 22:25:02 +0700

    Front end setup

--- app.js
+++ app.js
@@ -8,18 +8,17 @@ var bodyParser = require('body-parser');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'client', 'views'));
+app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-var routes = require('./routes/index');
+var routes = require('./routes');
 var api = require('./server/routes');
 
 app.use('/', routes);
