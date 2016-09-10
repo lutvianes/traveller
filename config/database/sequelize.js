e316e32fc78aeee6ec6e54bff0d22e4185d001e5
lutvianes <lutvianes@gmail.com>
2016-09-11 02:40:51 +0700

    Fix database env variables

--- config/database/sequelize.js
+++ config/database/sequelize.js
@@ -4,7 +4,8 @@ module.exports = {
         "username": process.env.TRAVELLER_DB_USERNAME,
         "password": process.env.TRAVELLER_DB_PASSWORD,
         "database": process.env.TRAVELLER_DB_DATABASE,
-        "host": process.env.TRAVELLER_DB_HOST,
+        "host": process.env.MYSQL_PORT_3306_TCP_ADDR,
+        "socketPath": '/var/run/mysqld/mysqld.sock',
         "dialect": "mysql",
         seederStorage: "sequelize"
     },
@@ -12,17 +13,19 @@ module.exports = {
         "username": process.env.TRAVELLER_DB_USERNAME,
         "password": process.env.TRAVELLER_DB_PASSWORD,
         "database": process.env.TRAVELLER_DB_DATABASE,
-        "host": process.env.TRAVELLER_DB_HOST,
+        "host": process.env.MYSQL_PORT_3306_TCP_ADDR,
+        "socketPath": '/var/run/mysqld/mysqld.sock',
         "dialect": "mysql",
         "logging": false,
         "force": true,
         seederStorage: "sequelize"
     },
     "production": {
-        "username": process.env.TRAVELLER_DB_USERNAME,
-        "password": process.env.TRAVELLER_DB_PASSWORD,
-        "database": process.env.TRAVELLER_DB_DATABASE,
-        "host": process.env.TRAVELLER_DB_HOST,
+        "username": process.env.MYSQL_ENV_MYSQL_USER,
+        "password": process.env.MYSQL_ENV_MYSQL_PASSWORD,
+        "database": process.env.MYSQL_ENV_MYSQL_DATABASE,
+        "host": process.env.MYSQL_PORT_3306_TCP_ADDR,
+        "socketPath": '/var/run/mysqld/mysqld.sock',
         "dialect": "mysql",
         seederStorage: "sequelize"
     }
